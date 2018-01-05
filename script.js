9316a9f8958e1678763c8388b7b9c844e1b76602
JMCJr <jorge.correa.jr@gmail.com>
2018-01-05 17:40:07 -0500

    fourth update

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 console.log('Lots of Java; lots of script.');
 
-function print() {  
-  board.render();
-  console.log('Mo Java; mo script.');
-}
+// function print() {  
+//   board.render();
+//   console.log('Mo Java; mo script.');
+// }
 
 var assStart = 1000;
 
 
 function countDown() {
+  if((document.querySelector('#assetCount').innerHTML) <= 0){
+    var box = document.querySelector('.board');
+    box.removeEventListener('click', selected);
+  }
   // var assCounter = document.querySelector('#assetCount');
   // var assNum = assStart - 100;
   // assCounter.innerHTML = assNum;
   // document.querySelector('#assetCount').innerHTML = assStart - 100;
 }
 
-function selected() {
-  event.target.classList.add('assignedAsset');
+function listenEvent(){
+  var box = document.querySelector('.board');
+  box.addEventListener('click', selected);
+}
+
+function selected(event) {
+  if(!event.target.classList.contains("assignedAsset")){
+    event.target.classList.add('assignedAsset');
+  // event.target.classList.remove('box');
   event.target.innerText = '100';
   document.querySelector('#assetCount').innerHTML = assStart -= 100;
-  
+  // var clickedBox = doument.querySelector('.assignedAsset');
+  // event.target.remove('.box');
+  // clickedBox.classList.add('taken');
+  // event.target.removeEventListener('click', selected);
+  } 
 }
 
 function renderRow(rowIdx) {
@@ -27,18 +42,36 @@ function renderRow(rowIdx) {
   rowEl.classList.add('row');
   for (var i = 0; i < 20; i++) {
     var box = document.createElement('div');
+    
+    // var row = Math.floor(rowIdx);
+    // var col = rowIdx;
+
+    // var boxVal = this.gridData[row][col];
+    // var boxShow = genCellDisplayClass(boxVal);
+
     box.classList.add('box');
-    box.addEventListener('click', selected);
+    box.dataset.row = rowIdx;
+    box.dataset.col = i;
+    // this.pushRow(rowIdx);
+    // this.pushCol(i);
+    // box.addEventListener('click', selected);
+    
     rowEl.appendChild(box);
+    // console.log(row);
+    // console.log(col);
   }
   return rowEl;
+  // return box;
+  // console.log(box);
 }
 
-function renderBoard() {
+
+function renderBoard(gridData) {
   var newElement = document.createElement('div');
   newElement.classList.add('board');
   for (var i = 0; i < 10; i++) {
     var newRow = this.renderRow(i);
+    this.gridData = gridData;
     newElement.appendChild(newRow);
   }
   return newElement;
@@ -47,6 +80,7 @@ function renderBoard() {
 function render() {
   var boardEl = document.querySelector('.board-holder');
   boardEl.innerHTML = '';
+  
   boardContent = this.renderBoard();
   boardEl.appendChild(boardContent);
 }
@@ -62,7 +96,18 @@ var board = {
   render: render,
   renderBoard: renderBoard,
   renderRow: renderRow,
+  listenEvent: listenEvent
 };
+
+// var think = {
+//   row: [],
+//   col: [],
+//   pushRow: pushRow(val),
+//   pushCol: pushCol(val)
+//   // pushRow: pushRow(val) {this.row.push(val)},
+//   // pushCol: pushCol(val) {this.col.push(val)}
+// };
     
-print();
-render();
+// print();
+board.render();
+board.listenEvent();
